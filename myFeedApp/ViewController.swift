54e32b819ea13f7dbf8db11f2e31acc41ad29909
hytkgami <gagagatrump@gmail.com>
2018-04-15 22:51:37 +0900

    Initial Commit

---
+++ myFeedApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myFeedApp
+//
+//  Created by Hajime Kagami on 2018/04/15.
+//  Copyright © 2018年 Hajime Kagami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
